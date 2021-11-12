3636618bc6508db47e51a63aae2b5acc909b19f8
tom-schier <schiertxs@gmail.com>
2021-11-12 18:23:44 +1100

    Updated path information

--- schemas/validate-schemas.js
+++ schemas/validate-schemas.js
@@ -2,7 +2,9 @@ const Ajv = require("ajv")
 var fs = require('fs');
 var path = require('path');
 
-const directoryPath = path.join(__dirname, 'common/1.14.0');
+
+const directoryPath = path.join(__dirname, 'banking/1.14.0');
+const commonDirectoryPath = path.join(__dirname, 'common/1.14.0');
 
 var dsbSchemas = [];
 fs.readdir(directoryPath, function (err, files) {
@@ -22,6 +24,22 @@ fs.readdir(directoryPath, function (err, files) {
     });
  });
 
+ fs.readdir(commonDirectoryPath, function (err, files) {
+  //handling error
+  if (err) {
+      return console.log('Unable to scan directory: ' + err);
+  } 
+  //listing all files using forEach
+  var i = 0;  
+  // create the schema array
+  files.forEach(function (file) {
+    var filePath = path.join(commonDirectoryPath, file);
+    var data = JSON.parse(fs.readFileSync(filePath));
+    data.$id = file;
+    dsbSchemas.push(data);
+    i++;  
+  });
+});
 
   try {
      fs.readdir(directoryPath, function (err, files) {
@@ -32,8 +50,7 @@ fs.readdir(directoryPath, function (err, files) {
         var data = JSON.parse(fs.readFileSync(filePath));
         var ajv = new Ajv({strictSchema: false}); 
         var validate = ajv.addSchema(dsbSchemas)
-        validate.compile(data);
-        
+        validate.compile(data);      
       });
       console.log("All done!")
     });
