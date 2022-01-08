5faf096582fd5a3a8511cd391d8af782de268407
tom-schier <schiertxs@gmail.com>
2022-01-08 14:38:55 +1100

    Updated Energy

--- validate-schemas.js
+++ validate-schemas.js
@@ -8,58 +8,49 @@ const addFormats = require("ajv-formats");
 const { Console } = require("console");
 
 
-//const sectors = ['banking', 'energy','register', 'admin', 'dcr'];
-const sectors = ['banking'];
+const sectors = ['banking', 'energy', 'register', 'admin', 'dcr'];
 const version = '1.15.0';
 
 sectors.forEach(sector => {
   const directoryPath = path.join(__dirname, version + '/schemas/' + sector);
-  const commonDirectoryPath = path.join(__dirname, version + '/schemas/common' );
+  const commonDirectoryPath = path.join(__dirname, version + '/schemas/common');
   var dsbSchemas = [];
   // Read the common schemas
   var commonFiles = fs.readdirSync(commonDirectoryPath);
- // fs.readdir(commonDirectoryPath, function (err, files) {
-    //handling error
-    // if (err) {
-    //   return console.log('Unable to scan directory: ' + err);
-    // }
-    
-    commonFiles.forEach(function (file) {
-      var filePath = path.join(commonDirectoryPath, file);
-      var data = JSON.parse(fs.readFileSync(filePath));
-      var fileName = file.substr(0, file.indexOf('.'));
-      data.$id = 'common/' + file;
-      dsbSchemas.push(data);
-    });
-    console.log("Added Common for " + sector + " validation");
- // });
+
+
+  commonFiles.forEach(function (file) {
+    var filePath = path.join(commonDirectoryPath, file);
+    var data = JSON.parse(fs.readFileSync(filePath));
+    //var fileName = file.substr(0, file.indexOf('.'));
+    data.$id = 'common/' + file;
+    dsbSchemas.push(data);
+  });
+  console.log("Added Common for " + sector + " validation");
+
   // Read the schemas to be validated
   var files = fs.readdirSync(directoryPath);
- // fs.readdirSync(directoryPath, function (err, files) {
-    //handling error
-    // if (err) {
-    //   return console.log('Unable to scan directory: ' + err);
-    // }
-    files.forEach(function (file) {
-      var filePath = path.join(directoryPath, file);
-      var data = JSON.parse(fs.readFileSync(filePath));
-      var fileName = file.substr(0, file.indexOf('.'));
-      data.$id = file;
-      dsbSchemas.push(data);
-    });
-    var ajv = new Ajv({ strictSchema: false });
-    addFormats(ajv);
-    // Configure the validator by adding the schema arrays
-    var validate = ajv.addSchema(dsbSchemas);
-    files.forEach(file => {
-      var filePath = path.join(directoryPath, file);
-      console.log('Processing file: ' + filePath);
-      var data = JSON.parse(fs.readFileSync(filePath));
-      validate.compile(data);
 
-    });
-    console.log("Validated " + sector);
- // });
+  files.forEach(function (file) {
+    var filePath = path.join(directoryPath, file);
+    var data = JSON.parse(fs.readFileSync(filePath));
+    //var fileName = file.substr(0, file.indexOf('.'));
+    data.$id = file;
+    dsbSchemas.push(data);
+  });
+  var ajv = new Ajv({ strictSchema: false });
+  addFormats(ajv);
+  // Configure the validator by adding the schema arrays
+  var validate = ajv.addSchema(dsbSchemas);
+  files.forEach(file => {
+    var filePath = path.join(directoryPath, file);
+    //console.log('Processing file: ' + filePath);
+    var data = JSON.parse(fs.readFileSync(filePath));
+    validate.compile(data);
+
+  });
+  console.log("Validated " + sector);
+
   console.log("All done for " + sector)
 });
 
